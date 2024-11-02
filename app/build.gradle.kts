7890ea7eb1ae5bc8e3cb52662c6dd297183001aa
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-11-02 13:37:42 -0300

    Ajustando API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.lifecycledemo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.lifecycledemo"
-        minSdk = 33
-        targetSdk = 34
+        minSdk = 30
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
